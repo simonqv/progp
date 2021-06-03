529cbdf5846fa39120a6770c9ef639e644c27127
simonqv <simonqv@kth.se>
2021-06-03 12:34:00 +0200

    COBOL DONE

--- COBOL/Bernoulli.cob
+++ COBOL/Bernoulli.cob
@@ -3,45 +3,53 @@ PROGRAM-ID. BERNOULLI.
 
 DATA DIVISION.
 WORKING-STORAGE SECTION.
-	77 bzero pic 9V9999.
-	77 bm pic 9V9999.
-	77 y pic 9V9999.
-	77 m pic 999.
-	77 k pic 999.
-	77 r pic 99V9999.
-	77 i pic 999.
-	77 testet pic 9V99.
+	77 bm pic S9(10)V9(15).
+	77 y pic S9(10)V9(15).
+	77 m pic 9(2).
+	77 k pic 9(2).
+	77 r pic S9(10)V9(15).
+	77 i pic 9(2).
+	77 disp pic S9(2)V9(7).
+	01 WS-TABLE.
+		05 B pic S9(10)V9(15) OCCURS 20 TIMES.
 
 
 
 PROCEDURE DIVISION.
-	move 1 to bzero
+	move 1 to B (1)
 	move 1 to m
-
-	COMPUTE testet = - 1 / 2
+	move B (1) to disp
+	DISPLAY disp
 		
-	move 0 to bm
-	perform until m greater than 20
+	
+	perform until m greater than 19
 		move 0 to y
+		move 0 to bm
 		move 0 to k
-		perform until k greater than (m - 1)
+		perform until k greater than (m + 1)
 			move 1 to r
+			
 			move 1 to i
 			perform until i greater than k
 				COMPUTE r = r * (m + 2 - i) / i
 				add 1 to i
 			end-perform
-
-			*> förmodligen bzeros som är fel????
-			COMPUTE y = bm - (r * bzero)
-			move bm to bzero
-			move y to bm
+					
+			
+			COMPUTE bm = bm - (r * B (k + 1))
+			
+			
 			add 1 to k
 
 		end-perform
-		COMPUTE y = y / (m + 1)
+		
+		COMPUTE y = bm / (m + 1)
+		
+		move y to B (m + 1)
 		add 1 to m
-		DISPLAY y
+		move y to disp
+		DISPLAY disp 
+
 	end-perform
 
 
